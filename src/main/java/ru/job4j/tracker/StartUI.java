618145c0427edcc4520f09a91f388179c8699c98
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-04-28 15:01:51 +0300

    5. Tracker - хранилище [#516043]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,8 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String createdFormat = item.getCreated().format(formatter);
         System.out.println(createdFormat);
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        System.out.println(tracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < rsl.length; index++) {
+            if (items[index] != null) {
+                rsl[size] = items[index];
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        size = 0;
+        return  rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < rsl.length; index++) {
+            if (items[index].getName().equals(key)) {
+                rsl[size] = items[index];
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, size);
+        size = 0;
+        return rsl;
     }
 
     public Item findById(int id) {
