cd0c1c6a980e6303ffb99044acfa388ca1dc7251
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-04-28 09:50:48 +0300

    2. Модель данных. [#516034]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Alexander Molchanov");
+        student.setGroup("#1");
+        student.setDate("1.1.2021");
+        System.out.println("Student " + student.getName() + " joined the group " + student.getGroup() + " " + student.getDate());
+    }
+}
