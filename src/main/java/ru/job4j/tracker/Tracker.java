ca57e79343b873598a08cbd894367c59cd355e10
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-04-29 11:29:25 +0300

    7. Метод удаления заявки Tracker.delete [#516039]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,12 +58,13 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
+        boolean rsl = false;
         if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index);
             items[size - 1] = null;
             size--;
-            return true;
+            rsl =  true;
         }
-        return false;
+        return rsl;
     }
 }
