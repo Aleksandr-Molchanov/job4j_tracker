a9f281f785e1ba76a6fb0bab43a5e42de8b8c1ef
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-05-04 18:31:57 +0300

    9.2. Тесты вывода на консоль [#515956]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -25,15 +25,22 @@ public class StartUITest {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Item name"));
+        String itemToString = "Item{id=1, name='Item name', created=" + item.getCreated() + '}';
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(item.getId()), "1"}
+                new String[] {"0", String.valueOf(item.getId())}
         );
         UserAction[] actions = {
                 new ShowAction(out),
                 new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "=== Show all items ====" + System.lineSeparator() +
+                        itemToString + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Exit" + System.lineSeparator()
+        ));
     }
 
     @Test
@@ -83,7 +90,13 @@ public class StartUITest {
                 new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).toString(), is(itemToString));
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "=== Find item by id ====" + System.lineSeparator() +
+                        itemToString + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Exit" + System.lineSeparator()
+        ));
     }
 
     @Test
@@ -91,6 +104,7 @@ public class StartUITest {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Item name"));
+        String itemToString = "Item{id=1, name='Item name', created=" + item.getCreated() + '}';
         Input in = new StubInput(
                 new String[] {"0", "Item name", "1"}
         );
@@ -99,7 +113,13 @@ public class StartUITest {
                 new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).getName(), is("Item name"));
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "=== Find items by name ====" + System.lineSeparator() +
+                        itemToString + System.lineSeparator() +
+                        "Menu." + System.lineSeparator() +
+                        "0. Exit" + System.lineSeparator()
+        ));
     }
 
     @Test
