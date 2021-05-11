088a31534e66905490eefe9749a25edeb328591b
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-05-11 17:14:18 +0300

    2. Singleton [#516000]

--- src/test/java/ru/job4j/tracker/SingleTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SingleTrackerTest.java
@@ -2,41 +2,20 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.core.IsNull.nullValue;
 
 public class SingleTrackerTest {
     @Test
-    public void whenAddNewItemThenSingleTrackerHasSameItem() {
-        SingleTracker tracker = new SingleTracker();
-        Item item = new Item();
-        item.setName("test1");
-        tracker.add(item);
-        Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
+    public void whenInstanceOne() {
+        SingleTracker first = SingleTracker.getInstance();
+        SingleTracker second = SingleTracker.getInstance();
+        assertThat(first, is(second));
     }
 
     @Test
-    public void whenReplace() {
-        SingleTracker tracker = new SingleTracker();
-        Item bug = new Item();
-        bug.setName("Bug");
-        tracker.add(bug);
-        int id = bug.getId();
-        Item bugWithDesc = new Item();
-        bugWithDesc.setName("Bug with description");
-        tracker.replace(id, bugWithDesc);
-        assertThat(tracker.findById(id).getName(), is("Bug with description"));
-    }
-
-    @Test
-    public void whenDelete() {
-        SingleTracker tracker = new SingleTracker();
-        Item bug = new Item();
-        bug.setName("Bug");
-        tracker.add(bug);
-        int id = bug.getId();
-        tracker.delete(id);
-        assertThat(tracker.findById(id), is(nullValue()));
+    public void whenInstanceNotNull() {
+        SingleTracker first = SingleTracker.getInstance();
+        assertNotNull(first);
     }
 }
