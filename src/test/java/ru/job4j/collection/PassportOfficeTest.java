3989808a8ee4af984d88576d2813d381b55714b3
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-05-19 09:39:26 +0300

    1. Паспорт и жители [#515945]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -2,6 +2,9 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -13,5 +16,15 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenDuplicateIsAdded() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        office.add(citizen2);
+        assertFalse(office.add(citizen2));
     }
 }
