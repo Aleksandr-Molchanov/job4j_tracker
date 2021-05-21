e1ccafd1202296a5600305165f01b6825ea92c8e
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-05-21 13:06:39 +0300

    0. Сортировка [#10096 #158729]

--- src/test/java/ru/job4j/tracker/SortBuNameItemTest.java
+++ src/test/java/ru/job4j/tracker/SortBuNameItemTest.java
@@ -1,6 +1,10 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -25,5 +29,29 @@ public class SortBuNameItemTest {
         SortBuNameItem sort = new SortBuNameItem();
         int result = sort.compare(new Item("a"), new Item("b"));
         assertThat(result, is(-1));
+    }
+
+    @Test
+    public void whenSortingInAscendingOrder() {
+        SortBuNameItem sort = new SortBuNameItem();
+        Item item1 = new Item(1, "b");
+        Item item2 = new Item(3, "c");
+        Item item3 = new Item(2, "a");
+        List<Item> items = Arrays.asList(item1, item2, item3);
+        List<Item> expect = Arrays.asList(item3, item1, item2);
+        items.sort(new SortBuNameItem());
+        assertEquals(items, expect);
+    }
+
+    @Test
+    public void whenSortingInDescendingOrder() {
+        SortBuNameItem sort = new SortBuNameItem();
+        Item item1 = new Item(1, "b");
+        Item item2 = new Item(3, "c");
+        Item item3 = new Item(2, "a");
+        List<Item> items = Arrays.asList(item1, item2, item3);
+        List<Item> expect = Arrays.asList(item2, item1, item3);
+        items.sort(new SortBuNameItem().reversed());
+        assertEquals(items, expect);
     }
 }
