b107ff3bda01bb177d92152a954113e64937de6b
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-05-22 19:56:40 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] l = left.split("\\.");
+        String[] r = right.split("\\.");
+        int numL = Integer.parseInt(l[0]);
+        int numR = Integer.parseInt(r[0]);
+        return numL - numR;
+    }
+}
