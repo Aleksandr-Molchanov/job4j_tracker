600f1649907392acb800fd78903e01de832dbd2f
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-05-25 21:00:18 +0300

    2. Отсортировать департаменты [#34352 #160606]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,20 +5,11 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl = 0;
         String[] first = o1.split("/");
         String[] second = o2.split("/");
-        int size = Math.min(first.length, second.length);
-        rsl = second[0].compareTo(first[0]);
-        if (rsl != 0) {
-            return rsl;
-        } else {
-            for (int index = 1; index < size; index++) {
-                rsl = first[index].compareTo(second[index]);
-            }
-            if (rsl == 0) {
-                rsl = Integer.compare(first.length, second.length);
-            }
+        int rsl = second[0].compareTo(first[0]);
+        if (rsl == 0) {
+            return o1.compareTo(o2);
         }
         return rsl;
     }
