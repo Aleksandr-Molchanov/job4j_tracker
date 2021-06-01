d69a33f0297a336eac01ecca46b182c2b11dd4e4
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-06-01 11:21:47 +0300

    1. Экзамен. Коллекции. [#10097 #160134]

--- src/main/java/ru/job4j/function/LambdaUsage.java
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -1,14 +1,19 @@
 package ru.job4j.function;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class LambdaUsage {
     public static void main(String[] args) {
+        List<String> strings = Arrays.asList("a", "aaa", "aa", "aaaa");
         Comparator<String> cmpDescSize = (left, right) -> {
             int first = left.length();
             int second = right.length();
             System.out.println("compare - " + left.length() + " : " + right.length());
             return Integer.compare(second, first);
         };
+        strings.sort(cmpDescSize);
+        System.out.println(strings);
     }
 }
