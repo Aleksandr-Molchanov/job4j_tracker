6bf5b9934a63d928710aa74b7851a5f7bb8ea824
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2021-06-07 09:18:23 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -11,7 +11,7 @@ public class DepartmentsTest {
     @Test
     public void whenMissed() {
         List<String> input = Collections.singletonList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -19,7 +19,7 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -27,7 +27,7 @@ public class DepartmentsTest {
     @Test
     public void sortAscMissedTest() {
         List<String> input = Arrays.asList("k2", "k1/sk2", "k2/sk1/ssk1");
-        List<String> expect = Arrays.asList("k1/sk2", "k2", "k2/sk1/ssk1");
+        List<String> expect = List.of("k1/sk2", "k2", "k2/sk1/ssk1");
         Departments.sortAsc(input);
         assertThat(input, is(expect));
     }
@@ -35,7 +35,7 @@ public class DepartmentsTest {
     @Test
     public void sortDescMissedTest() {
         List<String> input = Arrays.asList("k1/sk2", "k1", "k2/sk1/ssk1", "k2");
-        List<String> expect = Arrays.asList("k2", "k2/sk1/ssk1", "k1", "k1/sk2");
+        List<String> expect = List.of("k2", "k2/sk1/ssk1", "k1", "k1/sk2");
         Departments.sortDesc(input);
         assertThat(input, is(expect));
     }
